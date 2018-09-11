c363158aeb07ffed6fa5de01467b3c1703d7f24f
rishabhparsuram <rishabhparsuram@gmail.com>
2018-09-11 16:21:07 -0500

    Added code for maze solving algorithm

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,14 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                maze.turnRight();
+                while (maze.canMove() == false) {
+                    maze.turnLeft();
+                    maze.move();
+                }
+            }
+
         }
 
         if (maze.isFinished()) {
@@ -42,5 +49,6 @@ public class SolveMaze {
         } else {
             System.out.println("Try again!");
         }
+        System.out.println(maze);
     }
 }
